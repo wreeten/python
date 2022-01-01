e9f2f12bac8a492a7633856e0efa922dbd7e4396
wreeten <76799642+wreeten@users.noreply.github.com>
2021-12-31 23:27:45 -0800

    last commit before the new year!

---
+++ 12312021/last2021.py
@@ -0,0 +1,9 @@
+#data structures
+class Node:
+    def __init__(self,data):
+        self.data = data #assigning a data
+        self.next = None # intialioze next as null
+
+class linkedList:
+    def __init__(self):
+        self.head= None
